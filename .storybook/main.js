49f32faa324c558144ffd232f1cfa59de3419d56
Andre Almeida <andre-silva78@hotmail.com>
2022-03-05 11:02:29 -0300

    Fix storybook webpack to get the absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', "@storybook/addon-links", 'storybook-addon-next-router']
+  addons: ['@storybook/addon-essentials', "@storybook/addon-links", 'storybook-addon-next-router'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
