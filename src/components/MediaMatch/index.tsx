e5ab030ddbd629c3533eeecc94cfb7ac52274c95
Andre Almeida <andre-silva78@hotmail.com>
2022-03-08 22:25:52 -0300

    Create a helper to work better with media

---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from 'styled-media-query'
+
+type breakpoints = keyof DefaultBreakpoints
+
+export type MediaMatchProps = {
+  lessThan?: breakpoints
+  greaterThan?: breakpoints
+}
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoints) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+  greaterThan: (size: breakpoints) => css`
+    ${media.greaterThan(size)`display: block`}
+  `
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+  `}
+`
